fdc167ff00002c07c14b3b59670fa991f08cd9a9
deepparashar <deepparashar370@gmail.com>
2026-05-30 14:07:19 +0530

    Added PostOrder traversal code file

--- BinaryTree/PostOrderTraversal.java
+++ BinaryTree/PostOrderTraversal.java
@@ -1,9 +1,12 @@
 // In Pre-order traversal we have to print left first than right than root
 
-//In this i'm gonna write code in recursive as well as interative way using 2 stack.
+//In this i'm gonna write code in recursive way as well as in interative way using 2 stack.
 
 // First create Node class
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 class Node{ 
     int val;
@@ -38,8 +41,13 @@ public class PostOrderTraversal {
     root.right.left = new Node(6);
     root.right.right = new Node(7);
 
-    postOrder(root);
+    // postOrder(root);
+
+    List<Integer> result = PostOrder(root);
+    System.out.println(result);
     }
+
+        // Recursive way 
 
      public static void postOrder (Node root){
         // Check if root is empty so we return
@@ -55,4 +63,31 @@ public class PostOrderTraversal {
         // After that print root
         System.out.println(root.val + " ");
     }
+
+    // Iterative way using 2 stack
+    public static List<Integer> PostOrder(Node root){
+       Stack<Node> st1 = new Stack<Node>();
+       Stack <Node> st2 = new Stack<Node>();
+       List<Integer> postOrder = new ArrayList<>();
+
+       if(root == null) return postOrder;
+       st1.push(root);
+        
+       while(!st1.isEmpty()){
+         Node current  = st1.pop();
+         st2.push(current);
+
+         if(current.left != null) st1.push(current.left);
+         if(current.right != null) st1.push(current.right);
+       }
+       
+       while(!st2.isEmpty()){
+        postOrder.add(st2.pop().val);
+       }
+
+       return postOrder;
+    }
 }
+
+// Time complexity for this is O(N)
+// Space complexity for this is O(2N) cause we use 2 stack.
